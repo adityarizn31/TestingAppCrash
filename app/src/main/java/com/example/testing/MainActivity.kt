5f833701c86103b6e1b29c931a271a59d8de483b
[adityarn] <[adityarizkiawann@gmail.com]>
2024-10-10 09:35:16 +0700

    Testing 1

---
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.testing
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var btnSetValue: Button? = null
+    private lateinit var tvText: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue!!.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        if (view.id == R.id.btn_set_value) {
+            tvText.text = "19"
+        }
+    }
+}
